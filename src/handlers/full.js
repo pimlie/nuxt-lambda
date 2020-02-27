7ca20b3ca4ed5abcc70ee7a8f0f581f7c3eef1e2
pimlie <pimlie@hotmail.com>
2020-02-27 10:08:05 +0100

    chore: add test header

--- src/handlers/full.js
+++ src/handlers/full.js
@@ -11,7 +11,7 @@ const fullHandler = async (req, res) => {
 
   await nuxt.ready()
 
-  res.setHeader('Served by', 'nuxt-lambda')
+  res.setHeader('Served-by', 'nuxt-lambda')
 
   return nuxt.server.app(req, res)
 }
